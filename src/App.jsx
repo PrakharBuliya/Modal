b126805f5dd9eb3836994f6346dcb5b1651c133e
Prakhar Buliya <prakharbuliya02@gmail.com>
2023-09-23 14:50:32 +0530

    Added Animation

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const Modal = ({ show, onClose, title, children }) => {
     show && (
       <>
         <div className="modal-backdrop" onClick={onClose}></div>
-        <div className="modal-wrapper">
+        <div className={`modal-wrapper ${show ? "active" : ""}`}>
           <div className="modal-header">
             <div className="modal-title">{title}</div>
             <span className="modal-close" onClick={onClose}>
